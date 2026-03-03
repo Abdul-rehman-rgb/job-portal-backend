9ddc8ea5bc66ac680dfe512d2d2ba3b2d73a2d5a
Abdul Rehman <78441146+Abdul-rehman-rgb@users.noreply.github.com>
2026-03-04 02:30:43 +0500

    ad

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express();
 const corsOptions = {
   origin: ["https://job-portal-frontend-seven-sable.vercel.app"],
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 };
 
